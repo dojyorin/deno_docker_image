0ee926706be0410d4e1ce8d1851f9f21721e0a05
Kazuki Ota <ota_droid@live.jp>
2023-11-12 23:16:10 +0900

    WIP: version sync method.

--- script/compare.ts
+++ script/compare.ts
@@ -1,14 +1,3 @@
 import {parse, gt} from "https://deno.land/std@0.205.0/semver/mod.ts";
-import {fetchExtend} from "https://deno.land/x/simple_utility@v1.4.2/mod.ts";
-import {Endpoints} from "npm:@octokit/types@12.1.1";
 
-type LatestRelease = Endpoints["GET /repos/{owner}/{repo}/releases/latest"]["response"]["data"];
-
-async function getLatestRelease(repo:string){
-    return <LatestRelease>await fetchExtend(`https://api.github.com/repos/${repo}/releases/latest`, "json");
-}
-
-const {tag_name: tagCurrent} = await getLatestRelease(Deno.env.get("GITHUB_REPOSITORY") ?? "");
-const {tag_name: tagLatest} = await getLatestRelease("denoland/deno");
-
-const result = gt(parse(tagLatest), parse(tagCurrent));
+gt(parse(tagLatest), parse(tagCurrent));
---
+++ script/sync.sh
@@ -0,0 +1,10 @@
+function gh_tag {
+    curl -Ls "https://api.github.com/repos/${1}/releases/latest" | yq '.tag_name'
+}
+
+readonly origin='denoland/deno'
+readonly v_origin=$(gh_tag "${origin}")
+readonly v_local=$(gh_tag "${GITHUB_REPOSITORY}")
+
+git tag -a ${v_origin} -m "sync latest ${origin}"
+git push origin ${v_origin}
