a7464dae8b44133375ea9d83db4239bf1d59a54f
dojyorin <ota_droid@live.jp>
2023-11-16 14:03:56 +0900

    version replace.

--- script/sync.sh
+++ script/sync.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+set -eu
+
+cd ${0%/*}
+
 function gh_tag {
     curl -Ls "https://api.github.com/repos/${1}/releases/latest" | yq '.tag_name'
 }
@@ -5,11 +10,13 @@ function gh_tag {
 readonly origin='denoland/deno'
 
 readonly v_origin=$(gh_tag ${origin})
-readonly v_local=$(gh_tag ${GITHUB_REPOSITORY})
+readonly v_repo=$(gh_tag ${GITHUB_REPOSITORY})
 
-if [[ ${v_local} == ${v_origin} ]]; then
+if [[ ${v_repo} == ${v_origin} ]]; then
     exit
 fi
+
+sed -r -i -e "s/DENO_VERSION=\"v[0-9]+\.[0-9]+\.[0-9]+\"/DENO_VERSION=\"${v_origin}\"/" ../src/*.dockerfile
 
 git tag -a ${v_origin} -m "sync latest ${origin}"
 git push origin ${v_origin}
